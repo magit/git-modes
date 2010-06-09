2441fb8f2529ffc370862425a73c63fd4d53d46e
Florian Ragwitz <rafl@debian.org>
2010-06-10 00:03:16 +0200

    Add a license notice

--- git-commit.el
+++ git-commit.el
@@ -1,6 +1,9 @@
 ;;; git-commit.el --- Major mode for editing git commit messages
 
-;; Copyright (C) 2010 Florian Ragwitz
+;; This software is Copyright (c) 2010 by Florian Ragwitz.
+;;
+;; This is free software, licensed under:
+;;   The GNU General Public License, Version 2, June 1991
 
 ;; Author: Florian Ragwitz <rafl@debian.org>
 ;; Version: 0.0
