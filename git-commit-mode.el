4e7192163c86c25a57781daaa544adfeb4d037ba
Sebastian Wiesner <lunaryorn@gmail.com>
2012-10-03 11:18:33 +0200

    Add more comprehensive summary

--- git-commit-mode.el
+++ git-commit-mode.el
@@ -31,6 +31,34 @@
 
 ;; A major mode for editing Git commit messages.
 
+;; Formatting
+;; ----------
+;;
+;; Highlight the formatting of git commit messages and indicate errors according
+;; to the guidelines for commit messages (see
+;; http://tbaggery.com/2008/04/19/a-note-about-git-commit-messages.html).
+;;
+;; Highlight the first line (aka "summary") specially if it exceeds 54
+;; characters.
+
+;; Headers
+;; -------
+;;
+;; Provide commands to insert standard headers into commit messages.
+;;
+;; C-c C-s inserts Signed-off-by (`git-commit-signoff').
+;; C-C C-a inserts Acked-by (`git-commit-ack').
+;; C-c C-t inserts Tested-by (`git-commit-test').
+;; C-c C-r inserts Reviewed-by (`git-commit-review').
+;; C-c C-o inserts Cc (`git-commit-cc').
+;; C-c C-p inserts Reported-by (`git-commit-reported').
+
+;; Committing
+;; ----------
+;;
+;; C-c C-c finishes a commit.  By default this means to save and kill the
+;; buffer.  Customize `git-commit-commit-function' to change this behaviour.
+
 ;;; Code:
 
 (eval-when-compile
