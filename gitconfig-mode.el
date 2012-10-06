c5f784fe1e0175aecd7d4d90cac1e2395953ceb8
Sebastian Wiesner <lunaryorn@gmail.com>
2012-10-06 12:23:39 +0200

    Derive gitconfig-mode from conf-unix-mode
    Greatly simplifies the whole affair, but need to fix subsection
    highlighting, still

--- gitconfig-mode.el
+++ gitconfig-mode.el
@@ -30,62 +30,7 @@
 
 ;;; Code:
 
-(defgroup gitconfig nil
-  "Mode for editing git config files"
-  :group 'tools)
-
-(defgroup gitconfig-faces nil
-  "Faces for highlighting git config files"
-  :prefix "gitconfig-"
-  :group 'gitconfig
-  :group 'faces)
-
-(defface gitconfig-section-name-face
-  '((t (:inherit font-lock-function-name-face)))
-  "Face used to highlight the name of a section."
-  :group 'gitconfig-faces)
-
-(defface gitconfig-key-face
-  '((t (:inherit font-lock-keyword-face)))
-  "Face used to highlight the name of a key."
-  :group 'gitconfig-faces)
-
-(defface gitconfig-value-face
-  '((t (:inherit font-lock-string-face)))
-  "Face used to highlight the value of a key."
-  :group 'gitconfig-faces)
-
-(defvar gitconfig-font-lock-keywords
-  '(("^\s*\\[\\(.*\\)\\]\s*$" (1 'gitconfig-section-name-face t))
-    ("^\s*\\(.+?\\)\s*=\s*\\(.+\\)\s*$"
-     (1 'gitconfig-key-face)
-     (2 'gitconfig-value-face))))
-
-(defun gitconfig-beginning-of-section (&optional arg)
-  "Move point backwards to the beginning of a section.
-
-With ARG, do it that many times.  Negative ARG means move forward
-to the ARGth following beginning of a section.
-
-Return t if the movement was successful, or nil otherwise."
-  (interactive "^p")
-  (let ((repeat (or arg 1)))
-    (when (re-search-backward "^\s*\\(\\[.*\\]\\)\s*$" nil t repeat)
-      (goto-char (match-beginning 1))
-      t)))
-
-(defun gitconfig-end-of-section (&optional arg)
-  "Move point forward to next end of a section.
-
-With ARG, to it that many times.  Negative ARG means move
-backwards to the ARGth preceding end of a section.
-
-Return t if the movement was successful, or nil otherwise."
-  (interactive "^p")
-  (let ((repeat (or arg 1)))
-    (when (re-search-forward "\\(.\\)\n\s*\\[.*\\]\s*$" nil t repeat)
-      (goto-char (match-end 1))
-      t)))
+(require 'conf-mode)
 
 (defun gitconfig-indent-line ()
   "Indent the current line."
@@ -93,33 +38,23 @@ Return t if the movement was successful, or nil otherwise."
   (beginning-of-line)
   (delete-horizontal-space)
   (unless (looking-at "^\\[.*\\]\s*$")
-    (insert-tab)))
+    (insert-tab)
+    (back-to-indentation)))
+
+(defvar gitconfig-mode-syntax-table
+  (let ((table (make-syntax-table conf-unix-mode-syntax-table)))
+    ;; ; is a comment in .gitconfig
+    (modify-syntax-entry ?\; "<" table)
+    table)
+  "Syntax table to use in .gitconfig buffers.")
 
 ;;;###autoload
-(define-derived-mode gitconfig-mode prog-mode "Gitconfig"
+(define-derived-mode gitconfig-mode conf-unix-mode "Gitconfig"
   "A major mode for editing .gitconfig files."
-  ;; Setup font lock
-  (setq font-lock-defaults '(gitconfig-font-lock-keywords))
-  ;; Configure indentation
+  ;; .gitconfig is indented with tabs only
   (setq indent-tabs-mode t)
-  ;; Configure commenting syntax
-  (make-local-variable 'comment-start-skip)
-  (make-local-variable 'comment-start)
-  (make-local-variable 'comment-end)
-  (setq comment-start-skip "^#\s"
-        comment-start "# "
-        comment-end "")
-  ;; Section movement
-  (set (make-local-variable 'beginning-of-defun-function)
-       'gitconfig-beginning-of-section)
-  (set (make-local-variable 'end-of-defun-function)
-       'gitconfig-end-of-section)
-  ;; Indentation
   (set (make-local-variable 'indent-line-function)
        'gitconfig-indent-line))
-
-(modify-syntax-entry ?# "<" gitconfig-mode-syntax-table)
-(modify-syntax-entry ?\n ">" gitconfig-mode-syntax-table)
 
 ;;;###autoload
 (setq auto-mode-alist
