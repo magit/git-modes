52e5cd0688d892e4785f27823a8a37ef1097c70d
Jonas Bernoulli <jonas@bernoul.li>
2013-08-14 13:54:21 +0200

    rename from rebase-mode to git-rebase-mode
    Rename the mode from `rebase-mode' to `git-rebase-mode'.  Rename
    the file accordingly.  The prefix of most symbols is changed from
    "rebase-mode-" to "git-rebase-".  The prefix of a few other symbols
    is changed to "git-rebase-mode-" instead.

--- git-rebase-mode.el
+++ git-rebase-mode.el
@@ -1,4 +1,4 @@
-;;; rebase-mode -- edit git rebase files
+;;; git-rebase-mode -- Major mode for editing git rebase files
 
 ;; Copyright (C) 2010  Phil Jackson
 ;; Copyright (C) 2011  Peter J Weisberg
@@ -30,33 +30,33 @@
 (declare-function server-edit "server") 
 (require 'thingatpt)
 
-(defgroup rebase-mode nil
-  "Customize Rebase Mode"
+(defgroup git-rebase-mode nil
+  "Customize Git-Rebase mode"
   :group 'tools)
 
-(defcustom rebase-mode-auto-advance nil
+(defcustom git-rebase-auto-advance nil
   "If non-nil, moves point forward a line after running an action."
-  :group 'rebase-mode
+  :group 'git-rebase-mode
   :type 'boolean)
 
-(defgroup rebase-mode-faces nil
-  "Customize Rebase Mode faces."
+(defgroup git-rebase-mode-faces nil
+  "Customize Git-Rebase mode faces."
   :group 'faces
-  :group 'rebase-mode)
+  :group 'git-rebase-mode)
 
-(defface rebase-mode-killed-action-face
+(defface git-rebase-killed-action-face
   '((((class color))
      :inherit font-lock-comment-face
      :strike-through t))
   "Action lines in the rebase TODO list that have been commented out."
-  :group 'rebase-mode-faces)
+  :group 'git-rebase-mode-faces)
 
-(defface rebase-mode-description-face
+(defface git-rebase-description-face
   '((t :inherit font-lock-comment-face))
   "Face for one-line commit descriptions."
-  :group 'rebase-mode-faces)
+  :group 'git-rebase-mode-faces)
 
-(defconst rebase-mode-action-line-re
+(defconst git-rebase-action-line-re
   (rx
    line-start
    (? "#")
@@ -76,7 +76,7 @@
     (* not-newline)))
   "Regexp that matches an action line in a rebase buffer.")
 
-(defconst rebase-mode-exec-line-re
+(defconst git-rebase-exec-line-re
   (rx
    line-start
    (? "#")
@@ -88,24 +88,24 @@
     (* not-newline)))
   "Regexp that matches an exec line in a rebase buffer.")
 
-(defconst rebase-mode-dead-line-re
+(defconst git-rebase-dead-line-re
   (rx-to-string `(and line-start
                       (char ?#)
-                      (or (regexp ,(substring rebase-mode-action-line-re 1))
-                          (regexp ,(substring rebase-mode-exec-line-re 1)))) t)
+                      (or (regexp ,(substring git-rebase-action-line-re 1))
+                          (regexp ,(substring git-rebase-exec-line-re 1)))) t)
   "Regexp that matches a commented-out exec or action line in a rebase buffer.")
 
-(defvar rebase-mode-font-lock-keywords
+(defvar git-rebase-mode-font-lock-keywords
   (list
-   (list rebase-mode-action-line-re
+   (list git-rebase-action-line-re
          '(1 font-lock-keyword-face)
          '(2 font-lock-builtin-face)
-         '(3 'rebase-mode-description-face))
-   (list rebase-mode-exec-line-re
+         '(3 'git-rebase-description-face))
+   (list git-rebase-exec-line-re
          '(1 font-lock-keyword-face))
    (list (rx line-start (char "#") (* not-newline)) 0 font-lock-comment-face)
-   (list rebase-mode-dead-line-re 0 ''rebase-mode-killed-action-face t))
-  "Font lock keywords for `rebase-mode'.")
+   (list git-rebase-dead-line-re 0 ''git-rebase-killed-action-face t))
+  "Font lock keywords for Git-Rebase mode.")
 
 (defvar key-to-action-map
   '(("c" . "pick")
@@ -115,112 +115,112 @@
     ("f" . "fixup"))
   "Mapping from key to action.")
 
-(defvar rebase-mode-map
+(defvar git-rebase-mode-map
   (let ((map (make-sparse-keymap)))
     (define-key map (kbd "q") 'server-edit)
     (define-key map (kbd "C-c C-c") 'server-edit)
 
-    (define-key map (kbd "a") 'rebase-mode-abort)
-    (define-key map (kbd "C-c C-k") 'rebase-mode-abort)
+    (define-key map (kbd "a") 'git-rebase-abort)
+    (define-key map (kbd "C-c C-k") 'git-rebase-abort)
 
-    (define-key map (kbd "RET") 'rebase-mode-show-commit)
+    (define-key map (kbd "RET") 'git-rebase-show-commit)
 
-    (define-key map (kbd "M-p") 'rebase-mode-move-line-up)
-    (define-key map (kbd "M-n") 'rebase-mode-move-line-down)
-    (define-key map (kbd "k") 'rebase-mode-kill-line)
-    (define-key map (kbd "x") 'rebase-mode-exec)
+    (define-key map (kbd "M-p") 'git-rebase-move-line-up)
+    (define-key map (kbd "M-n") 'git-rebase-move-line-down)
+    (define-key map (kbd "k") 'git-rebase-kill-line)
+    (define-key map (kbd "x") 'git-rebase-exec)
 
     (define-key map (kbd "n") 'forward-line)
-    (define-key map (kbd "p") 'rebase-mode-backward-line)
-    (define-key map [remap undo] 'rebase-mode-undo)
+    (define-key map (kbd "p") 'git-rebase-backward-line)
+    (define-key map [remap undo] 'git-rebase-undo)
     map)
-  "Keymap for rebase-mode.
+  "Keymap for Git-Rebase mode.
 Note this will be added to by the top-level code which defines
 the edit functions.")
 
-(easy-menu-define rebase-mode-menu rebase-mode-map
-  "Rebase-mode menu"
+(easy-menu-define git-rebase-mode-menu git-rebase-mode-map
+  "Git-Rebase mode menu"
   '("Rebase"
-    ["Pick" rebase-mode-pick t]
-    ["Reword" rebase-mode-reword t]
-    ["Edit" rebase-mode-edit t]
-    ["Squash" rebase-mode-squash t]
-    ["Fixup" rebase-mode-fixup t]
-    ["Kill" rebase-mode-kill-line t]
-    ["Move Down" rebase-mode-move-line-down t]
-    ["Move Up" rebase-mode-move-line-up t]
-    ["Execute" rebase-mode-exec t]
+    ["Pick" git-rebase-pick t]
+    ["Reword" git-rebase-reword t]
+    ["Edit" git-rebase-edit t]
+    ["Squash" git-rebase-squash t]
+    ["Fixup" git-rebase-fixup t]
+    ["Kill" git-rebase-kill-line t]
+    ["Move Down" git-rebase-move-line-down t]
+    ["Move Up" git-rebase-move-line-up t]
+    ["Execute" git-rebase-exec t]
     "---"
-    ["Abort" rebase-mode-abort t]
+    ["Abort" git-rebase-abort t]
     ["Done" server-edit t]))
 
 ;; create the functions which edit the action lines themselves (based
 ;; on `key-to-action-map' above)
 (mapc (lambda (key-action)
-        (let ((fun-name (intern (concat "rebase-mode-" (cdr key-action)))))
+        (let ((fun-name (intern (concat "git-rebase-" (cdr key-action)))))
           ;; define the function
           (eval `(defun ,fun-name ()
                    (interactive)
-                   (rebase-mode-edit-line ,(cdr key-action))))
+                   (git-rebase-edit-line ,(cdr key-action))))
 
-          ;; bind the function in `rebase-mode-map'
-          (define-key rebase-mode-map (car key-action) fun-name)))
+          ;; bind the function in `git-rebase-mode-map'
+          (define-key git-rebase-mode-map (car key-action) fun-name)))
       key-to-action-map)
 
-(defun rebase-mode-edit-line (change-to)
+(defun git-rebase-edit-line (change-to)
   "Change the keyword at the start of the current action line to
 that of CHANGE-TO."
-  (when (rebase-mode-looking-at-action)
+  (when (git-rebase-looking-at-action)
     (let ((buffer-read-only nil)
           (start (point)))
       (goto-char (point-at-bol))
       (delete-region (point) (progn (forward-word 1) (point)))
       (insert change-to)
       (goto-char start)
-      (when rebase-mode-auto-advance
+      (when git-rebase-auto-advance
         (forward-line)))))
 
-(defun rebase-mode-looking-at-action ()
+(defun git-rebase-looking-at-action ()
   "Return non-nil if looking at an action line."
   (save-excursion
     (goto-char (point-at-bol))
-    (looking-at rebase-mode-action-line-re)))
+    (looking-at git-rebase-action-line-re)))
 
-(defun rebase-mode-looking-at-action-or-exec ()
+(defun git-rebase-looking-at-action-or-exec ()
   "Return non-nil if looking at an action line or exec line."
   (save-excursion
     (goto-char (point-at-bol))
-    (or (looking-at rebase-mode-action-line-re)
-        (looking-at rebase-mode-exec-line-re))))
+    (or (looking-at git-rebase-action-line-re)
+        (looking-at git-rebase-exec-line-re))))
 
-(defun rebase-mode-looking-at-exec ()
+(defun git-rebase-looking-at-exec ()
   "Return non-nil if cursor is on an exec line."
-  (string-match rebase-mode-exec-line-re (thing-at-point 'line)))
+  (string-match git-rebase-exec-line-re (thing-at-point 'line)))
 
-(defun rebase-mode-looking-at-killed-exec ()
+(defun git-rebase-looking-at-killed-exec ()
   "Return non-nil if looking at an exec line that has been commented out."
   (let ((line (thing-at-point 'line)))
     (and (eq (aref line 0) ?#)
-         (string-match rebase-mode-exec-line-re line))))
+         (string-match git-rebase-exec-line-re line))))
 
-(defun rebase-mode-move-line-up ()
+(defun git-rebase-move-line-up ()
   "Move the current action line up."
   (interactive)
-  (when (rebase-mode-looking-at-action-or-exec)
+  (when (git-rebase-looking-at-action-or-exec)
     (let ((buffer-read-only nil)
           (col (current-column)))
       (transpose-lines 1)
       (forward-line -2)
       (move-to-column col))))
 
-(defun rebase-mode-move-line-down ()
+(defun git-rebase-move-line-down ()
   "Assuming the next line is also an action line, move the current line down."
   (interactive)
   ;; if we're on an action and the next line is also an action
-  (when (and (rebase-mode-looking-at-action-or-exec)
+  (when (and (git-rebase-looking-at-action-or-exec)
              (save-excursion
                (forward-line)
-               (rebase-mode-looking-at-action-or-exec)))
+               (git-rebase-looking-at-action-or-exec)))
     (let ((buffer-read-only nil)
           (col (current-column)))
       (forward-line 1)
@@ -228,7 +228,7 @@ that of CHANGE-TO."
       (forward-line -1)
       (move-to-column col))))
 
-(defun rebase-mode-abort ()
+(defun git-rebase-abort ()
   "Abort this rebase.
 This is dune by emptying the buffer, saving and closing server
 connection."
@@ -240,17 +240,17 @@ connection."
       (save-buffer)
       (server-edit))))
 
-(defun rebase-mode-kill-line ()
+(defun git-rebase-kill-line ()
   "Kill the current action line."
   (interactive)
   (when (and (not (eq (char-after (point-at-bol)) ?#))
-             (rebase-mode-looking-at-action-or-exec))
+             (git-rebase-looking-at-action-or-exec))
     (beginning-of-line)
     (let ((buffer-read-only nil))
       (insert "#"))
     (forward-line)))
 
-(defun rebase-mode-exec (edit)
+(defun git-rebase-exec (edit)
   "Prompt the user for a shell command to be executed, and
 add it to the todo list.
 
@@ -262,8 +262,8 @@ exec line, edit that line instead of inserting a new one.  If the
 exec line was commented out, also uncomment it."
   (interactive "P")
   (cond
-   ((and edit (rebase-mode-looking-at-exec))
-    (let ((new-line (rebase-mode-read-exec-line
+   ((and edit (git-rebase-looking-at-exec))
+    (let ((new-line (git-rebase-read-exec-line
                      (match-string-no-properties 2 (thing-at-point 'line))))
           (inhibit-read-only t))
       (delete-region (point-at-bol) (point-at-eol))
@@ -272,57 +272,57 @@ exec line was commented out, also uncomment it."
         (delete-char -1)
         (forward-line))
       (move-beginning-of-line nil)))
-   ((rebase-mode-looking-at-killed-exec)
+   ((git-rebase-looking-at-killed-exec)
     (save-excursion
       (beginning-of-line)
       (let ((buffer-read-only nil))
         (delete-char 1))))
    (t
     (let ((inhibit-read-only t)
-          (line (rebase-mode-read-exec-line)))
+          (line (git-rebase-read-exec-line)))
       (unless (equal "" line)
         (move-end-of-line nil)
         (newline)
         (insert (concat "exec " line))))
     (move-beginning-of-line nil))))
 
-(defun rebase-mode-read-exec-line (&optional initial-line)
+(defun git-rebase-read-exec-line (&optional initial-line)
   (read-shell-command "Execute: " initial-line))
 
-(defun rebase-mode-undo (&optional arg)
+(defun git-rebase-undo (&optional arg)
   "A thin wrapper around `undo', which allows undoing in read-only buffers."
   (interactive "P")
   (let ((inhibit-read-only t))
     (undo arg)))
 
-(defun rebase-mode-show-commit (&optional arg)
+(defun git-rebase-show-commit (&optional arg)
   "Show the commit on the current line if any."
   (interactive "P")
   (save-excursion
     (goto-char (point-at-bol))
-    (when (looking-at rebase-mode-action-line-re)
+    (when (looking-at git-rebase-action-line-re)
       (let ((commit (match-string 2)))
         (if (fboundp 'magit-show-commit)
             (magit-show-commit commit nil nil 'select)
           (shell-command (concat "git show " commit)))))))
 
-(defun rebase-mode-backward-line (&optional n)
+(defun git-rebase-backward-line (&optional n)
   "Move N lines backward (forward if N is negative).
 Like `forward-line' but go into the opposite direction."
   (interactive "p")
   (forward-line (* n -1)))
 
 ;;;###autoload
-(define-derived-mode rebase-mode special-mode "Rebase"
+(define-derived-mode git-rebase-mode special-mode "Rebase"
   "Major mode for editing of a Git rebase file.
 
 Rebase files are generated when you run 'git rebase -i' or run
 `magit-interactive-rebase'.  They describe how Git should perform
 the rebase.  See the documentation for git-rebase (e.g., by
 running 'man git-rebase' at the command line) for details."
-  (setq font-lock-defaults '(rebase-mode-font-lock-keywords t t)))
+  (setq font-lock-defaults '(git-rebase-mode-font-lock-keywords t t)))
 
-(defun rebase-mode-show-keybindings ()
+(defun git-rebase-show-keybindings ()
   "Modify the \"Commands:\" section of the comment Git generates
 at the bottom of the file so that in place of the one-letter
 abbreviation for the command, it shows the command's keybinding.
@@ -332,23 +332,23 @@ By default, this is the same except for the \"pick\" command."
     (while (search-forward-regexp "^#  \\(.\\), \\([[:alpha:]]+\\) = " nil t)
       (let ((start (match-beginning 1))
             (end (match-end 1))
-            (command (intern (concat "rebase-mode-" (match-string 2)))))
+            (command (intern (concat "git-rebase-" (match-string 2)))))
         (when (fboundp command)
           (let ((overlay (make-overlay start end)))
             (overlay-put
              overlay 'display
              (key-description (where-is-internal command nil t)))))))))
 
-(add-hook 'rebase-mode-hook 'rebase-mode-show-keybindings t)
+(add-hook 'git-rebase-mode-hook 'git-rebase-mode-show-keybindings t)
 
-(defun rebase-mode-disable-before-save-hook ()
+(defun git-rebase-mode-disable-before-save-hook ()
   (set (make-local-variable 'before-save-hook) nil))
 
-(add-hook 'rebase-mode-hook 'rebase-mode-disable-before-save-hook)
+(add-hook 'git-rebase-mode-hook 'git-rebase-mode-disable-before-save-hook)
 
 ;;;###autoload
 (add-to-list 'auto-mode-alist
-             '("git-rebase-todo" . rebase-mode))
+             '("git-rebase-todo" . git-rebase-mode))
 
-(provide 'rebase-mode)
-;;; rebase-mode.el ends here
+(provide 'git-rebase-mode)
+;;; git-rebase-mode.el ends here
