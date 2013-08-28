bc4750e0b2fa05f7efec752a136922f61f864e00
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2013-08-28 13:29:47 +0200

    Add mode hooks to {git-commit,gitconfig,gitignore}-mode
    I only actually needed this for the git-commit-mode, because I wanted to
    turn off the annoying auto-fill, I prefer to do it myself, and sometimes
    you want long lines for e.g. quoting log output.
    
    So now you can do that like:
    
        (add-hook 'git-commit-mode-hook
                  '(lambda ()
                     (message "Running the git commit mode hook")
                     (turn-off-auto-fill)))
    
    I didn't add a git-rebase-mode-hook because it's already here in some
    form, although I don't understand how it's supposed to work:
    
        $ git --no-pager grep git-rebase-mode-hook
        git-rebase-mode.el:362:(add-hook 'git-rebase-mode-hook 'git-rebase-mode-show-keybindings t)
        git-rebase-mode.el:367:(add-hook 'git-rebase-mode-hook 'git-rebase-mode-disable-before-save-hook)

--- git-commit-mode.el
+++ git-commit-mode.el
@@ -144,6 +144,9 @@ confirmation before committing."
   "Hook run by `git-commit-commit' unless clients exist.
 Only use this if you know what you are doing.")
 
+(defvar git-commit-mode-hook nil
+  "List of functions to be called when activating `git-commit-mode'.")
+
 (defun git-commit-commit (&optional force)
   "Finish editing the commit message and commit.
 
@@ -511,7 +514,9 @@ basic structure of and errors in git commit messages."
        (concat paragraph-start "\\|*\\|("))
   ;; Do not remember point location in commit messages
   (when (fboundp 'toggle-save-place)
-    (toggle-save-place 0)))
+    (toggle-save-place 0))
+  ;; Run any user-defined hooks as the last thing we do.
+  (run-mode-hooks 'git-commit-mode-hook))
 
 ;;;###autoload
 (dolist (pattern '("/COMMIT_EDITMSG\\'" "/NOTES_EDITMSG\\'"
