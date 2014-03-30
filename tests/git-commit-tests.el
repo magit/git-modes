a3f336729e915c835a227e3fe80fffe365ccc548
Jonas Bernoulli <jonas@bernoul.li>
2014-03-30 16:57:39 +0200

    git-commit-tests.el: cleanup

--- tests/git-commit-tests.el
+++ tests/git-commit-tests.el
@@ -1,19 +1,27 @@
-(require 'cl)
+;;; git-commit-tests.el --- tests for `git-commit-mode.el'
+
+;; Copyright (C) 2013-2014  The Magit Project Developers
+;;
+;; License: GPLv3
+
+;;; Code:
+
+(require 'cl-lib)
 (require 'ert)
 
 (require 'git-commit-mode)
 
-;;; tests
+;;; Utilities
 
 (defmacro git-commit-with-temp-buffer (&rest body)
   "Like `with-temp-buffer', but put the buffer in `git-commit' mode."
+  (declare (indent defun))
   `(with-temp-buffer
      (git-commit-mode)
      (erase-buffer)                   ; `git-commit-mode' adds newline
      (remove-hook 'kill-buffer-query-functions
                   'with-editor-kill-buffer-noop t)
      ,@body))
-
 
 (defmacro git-commit-message-end-position-test (test-name msg comment)
   "Make a `git-commit-message-end-position' test."
@@ -27,6 +35,17 @@
                        1 (git-commit-find-pseudo-header-position))
                       ,msg)))))
 
+(defmacro git-commit-with-temp-message-history (&rest body)
+  `(let ((log-edit-comment-ring (make-ring log-edit-maximum-comment-ring-size))
+         (log-edit-comment-ring-index nil))
+    (ring-insert log-edit-comment-ring "msg one\n\n")
+    (ring-insert log-edit-comment-ring "msg two\n\n")
+    (ring-insert log-edit-comment-ring "msg three\n\n")
+    (git-commit-with-temp-buffer
+      ,@body)))
+
+;;; Tests
+
 (git-commit-message-end-position-test msg+nl+comment
   "msg\n\n" "#comment\n")
 (git-commit-message-end-position-test msg+comment
@@ -35,19 +54,6 @@
   "msg\n\n" "#comment\n\n#more comment\n")
 (git-commit-message-end-position-test nl+comment
   "\n\n" "#comment\n\n#more comment\n")
-
-(defconst git-commit-test-message-history
-  (let ((ring (make-ring log-edit-maximum-comment-ring-size)))
-    (ring-insert ring "msg one\n\n")
-    (ring-insert ring "msg two\n\n")
-    (ring-insert ring "msg three\n\n")
-    ring))
-
-(defmacro git-commit-with-temp-message-history (&rest body)
-  `(let ((log-edit-comment-ring (ring-copy git-commit-test-message-history))
-         (log-edit-comment-ring-index nil))
-     (git-commit-with-temp-buffer
-       ,@body)))
 
 (ert-deftest git-commit-message-history-leave-comments ()
   "History cycling commands should not affect comments"
@@ -62,3 +68,9 @@
    (insert "\n\n#comment")
    (git-commit-prev-message 1)
    (should (equal (buffer-string) "msg three\n\n#comment"))))
+
+(provide 'git-commit-tests)
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; End:
+;;; git-commit-tests.el ends here
