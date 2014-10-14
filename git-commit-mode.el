84cbfbe81dc8ac9f9217e065b9c24b970fa666b7
Vasily Korytov <vasily.korytov@rochsystems.com>
2014-10-14 23:59:06 +0300

    don't use dolist to add entries to auto-mode-alist
    See #110 and #111.

--- git-commit-mode.el
+++ git-commit-mode.el
@@ -650,10 +650,11 @@ basic structure of and errors in git commit messages."
         'git-commit-mode-flyspell-verify))
 
 ;;;###autoload
-(dolist (pattern '("/COMMIT_EDITMSG\\'" "/NOTES_EDITMSG\\'"
-                   "/MERGE_MSG\\'" "/TAG_EDITMSG\\'"
-                   "/PULLREQ_EDITMSG\\'"))
-  (add-to-list 'auto-mode-alist (cons pattern 'git-commit-mode)))
+(add-to-list 'auto-mode-alist '("/MERGE_MSG\\'" . git-commit-mode))
+;;;###autoload
+(add-to-list 'auto-mode-alist
+             '("/\\(?:COMMIT\\|NOTES\\|TAG\\|PULLREQ\\)_EDITMSG\\'"
+               . git-commit-mode))
 
 (defun git-commit-auto-mode-enable ()
   (message "git-commit-auto-mode-enable is obsolete and doesn't do anything"))
