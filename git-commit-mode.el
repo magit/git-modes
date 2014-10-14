9b168c49c053c68e3799b277ed7b2bc2f1c52f1e
Vasily Korytov <vasily.korytov@rochsystems.com>
2014-10-14 23:59:06 +0300

    regexp, not the loop

--- git-commit-mode.el
+++ git-commit-mode.el
@@ -650,10 +650,8 @@ basic structure of and errors in git commit messages."
         'git-commit-mode-flyspell-verify))
 
 ;;;###autoload
-(dolist (pattern '("/COMMIT_EDITMSG\\'" "/NOTES_EDITMSG\\'"
-                   "/MERGE_MSG\\'" "/TAG_EDITMSG\\'"
-                   "/PULLREQ_EDITMSG\\'"))
-  (add-to-list 'auto-mode-alist (cons pattern 'git-commit-mode)))
+(add-to-list 'auto-mode-alist '("/\\(?:\\(?:COMMIT\\|NOTES\\|TAG\\|PULLREQ\\)_EDIT\\|MERGE_\\)MSG\\'"
+                                . git-commit-mode))
 
 (defun git-commit-auto-mode-enable ()
   (message "git-commit-auto-mode-enable is obsolete and doesn't do anything"))
