77ddaf2df6101e33cfe24c1709a8de68373c1ba6
Jonas Bernoulli <jonas@bernoul.li>
2014-10-14 17:56:17 +0200

    with-editor.el: update commentary

--- with-editor.el
+++ with-editor.el
@@ -28,8 +28,43 @@
 ;; provided, which communicates with Emacs on stdout instead of using
 ;; a socket as the Emacsclient does.
 
-;; Additionally `with-editor-mode' provides some utilities that make
-;; it nicer to specialize edit sessions.
+;; The commands `with-editor-async-shell-command' and
+;; `with-editor-shell-command' are intended as drop in replacements
+;; for `async-shell-command' and `shell-command'.  They automatically
+;; export $EDITOR making sure the executed command uses the current
+;; Emacs instance as "the editor".  With a prefix argument these
+;; commands prompt for an alternative environment variable such as
+;; $GIT_EDITOR.  To always use these variants add this to you init
+;; file:
+;;
+;;   (define-key (current-global-map)
+;;     [remap async-shell-command] 'with-editor-async-shell-command)
+;;   (define-key (current-global-map)
+;;     [remap shell-command] 'with-editor-shell-command)
+
+;; Alternatively use the global `shell-command-with-editor-mode',
+;; which always sets $EDITOR for all Emacs commands which ultimately
+;; use `shell-command' to asynchronously some shell command.
+
+;; The command `with-editor-export-editor' exports $EDITOR or another
+;; such environment variable in `shell-mode', `term-mode' and
+;; `eshell-mode' buffers.  Use this Emacs command before executing a
+;; shell command which needs "the editor" set, or always arrange for
+;; the current Emacs instance to be used as editor by adding it to
+;; the appropriate mode hooks:
+;;
+;;   (add-hook 'shell-mode-hook  'with-editor-export-editor)
+;;   (add-hook 'term-mode-hook   'with-editor-export-editor)
+;;   (add-hook 'eshell-mode-hook 'with-editor-export-editor)
+
+;; This library can also be used by other packages which need to use
+;; the current Emacs instance as editor.  In fact this library was
+;; written for `git-commit-mode' and `git-rebase-mode', which in turn
+;; are used by Magit.  These two packages also use and extend
+;; `with-editor-mode', which isn't used by end-user functionality
+;; defined here.  Consult `git-rebase-mode' and the related code in
+;; `magit-sequence' for a simple example, `git-commit-mode' is more
+;; complex.
 
 ;;; Code:
 
