75d9bfbae3e8ae735220256bad6c779693c5ff21
Syohei YOSHIDA <syohex@gmail.com>
2016-03-02 19:08:46 +0900

    Fix infinite-loop issue
    If cursor is end-of-buffer, then infinite-loop occurs.

--- gitattributes-mode.el
+++ gitattributes-mode.el
@@ -141,7 +141,7 @@ If NO-STATE is non-nil then do not print state."
      (let ((old-limit limit))
        (save-excursion
          (beginning-of-line)
-         (while (looking-at "^\\s-*$")
+         (while (and (not (eobp)) (looking-at "^\\s-*$"))
            (forward-line))
          (when (re-search-forward "[[:space:]]" limit 'noerror)
            (setq limit (point))))
