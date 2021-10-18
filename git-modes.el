9cbdac7dcfd340afc018d38f71ac5a73ac4dbcec
Jonas Bernoulli <jonas@bernoul.li>
2021-10-18 18:37:27 +0200

    Remove the "not part of GNU Emacs" notices

--- git-modes.el
+++ git-modes.el
@@ -13,8 +13,6 @@
 ;; Package-Requires: ((emacs "24.3"))
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
-;; This file is not part of GNU Emacs.
-
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 3, or (at your option)
--- gitattributes-mode.el
+++ gitattributes-mode.el
@@ -10,8 +10,6 @@
 ;; Package-Requires: ((emacs "24.3"))
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
-;; This file is NOT part of GNU Emacs.
-
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
