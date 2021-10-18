d84f29aa234b5a0f6e3ecd4ea4706b457d0e957a
Jonas Bernoulli <jonas@bernoul.li>
2021-10-18 18:43:30 +0200

    Use same permission statement in all libraries
    Talk about the "file", say "of the License" and unify punctuation.

--- gitconfig-mode.el
+++ gitconfig-mode.el
@@ -11,9 +11,9 @@
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
 ;; This file is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
+;; it under the terms of the GNU General Public License as published
+;; by the Free Software Foundation; either version 3 of the License,
+;; or (at your option) any later version.
 
 ;; This file is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
