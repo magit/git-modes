845d333732ad9c8d65a28f12af645c6682db6f45
Jonas Bernoulli <jonas@bernoul.li>
2022-04-22 18:11:06 +0200

    Refresh library header

--- git-modes.el
+++ git-modes.el
@@ -1,8 +1,8 @@
-;;; git-modes.el --- Major modes for editing Git configuration files  -*- lexical-binding: t -*-
+;;; git-modes.el --- Major modes for editing Git configuration files  -*- lexical-binding:t -*-
 
-;; Copyright (c) 2012-2013  Sebastian Wiesner
-;; Copyright (C) 2013-2015  Rüdiger Sonderfeld
-;; Copyright (C) 2012-2022  The Magit Project Contributors
+;; Copyright (c) 2012-2013 Sebastian Wiesner
+;; Copyright (C) 2013-2015 Rüdiger Sonderfeld
+;; Copyright (C) 2012-2022 The Magit Project Contributors
 
 ;; Author: Sebastian Wiesner <lunaryorn@gmail.com>
 ;;         Rüdiger Sonderfeld <ruediger@c-plusplus.net>
@@ -10,22 +10,24 @@
 ;; Maintainer: Jonas Bernoulli <jonas@bernoul.li>
 ;; Homepage: https://github.com/magit/git-modes
 ;; Keywords: convenience vc git
-;; Package-Requires: ((emacs "25.1"))
+
 ;; Package-Version: 1.4.0
+;; Package-Requires: ((emacs "25.1"))
+
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
-;; This file is free software; you can redistribute it and/or modify
+;; This file is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published
-;; by the Free Software Foundation; either version 3 of the License,
+;; by the Free Software Foundation, either version 3 of the License,
 ;; or (at your option) any later version.
-
+;;
 ;; This file is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
-;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
+;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
--- gitattributes-mode.el
+++ gitattributes-mode.el
@@ -1,26 +1,27 @@
-;;; gitattributes-mode.el --- Major mode for editing .gitattributes files  -*- lexical-binding: t -*-
+;;; gitattributes-mode.el --- Major mode for editing .gitattributes files  -*- lexical-binding:t -*-
 
-;; Copyright (C) 2013-2015  Rüdiger Sonderfeld
-;; Copyright (C) 2013-2021  The Magit Project Contributors
+;; Copyright (C) 2013-2015 Rüdiger Sonderfeld
+;; Copyright (C) 2013-2022 The Magit Project Contributors
 
 ;; Author: Rüdiger Sonderfeld <ruediger@c-plusplus.net>
 ;; Maintainer: Jonas Bernoulli <jonas@bernoul.li>
 ;; Homepage: https://github.com/magit/git-modes
 ;; Keywords: convenience vc git
+
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
-;; This file is free software; you can redistribute it and/or modify
+;; This file is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published
-;; by the Free Software Foundation; either version 3 of the License,
+;; by the Free Software Foundation, either version 3 of the License,
 ;; or (at your option) any later version.
-
+;;
 ;; This file is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
-;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
+;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
