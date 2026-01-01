5e50992875c8bfd1192bbfa8d742f6e81b2fe6eb
Jonas Bernoulli <jonas@bernoul.li>
2026-01-01 17:30:24 +0100

    Improve indentation of cond and interactive
    Emacs 31.1 adds variable `lisp-indent-local-overrides'.

--- gitattributes-mode.el
+++ gitattributes-mode.el
@@ -121,10 +121,10 @@ If NO-STATE is non-nil then do not print state."
                                          gitattributes-mode-attributes)))
       (concat (unless no-state
                 (cond
-                 ((string= (match-string 1) "-") "[Unset] ")
-                 ((string= (match-string 1) "!") "[Unspecified] ")
-                 ((string= (match-string 3) "=") "[Set to a value] ")
-                 (t "[Set] ")))
+                  ((string= (match-string 1) "-") "[Unset] ")
+                  ((string= (match-string 1) "!") "[Unspecified] ")
+                  ((string= (match-string 3) "=") "[Set to a value] ")
+                  (t "[Set] ")))
               (cadr entry)))))
 
 (defvar gitattributes-mode-syntax-table
